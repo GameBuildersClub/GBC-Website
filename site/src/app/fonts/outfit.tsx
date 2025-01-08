5659b6a1e98489e10b965d3e766b51e08777e004
Opuhlos <140357050+Opuhlos@users.noreply.github.com>
2025-01-07 22:35:19 -0500

    Added Outfit font. Progress made on banner for frequently-asked-questions page.

---
+++ site/src/app/fonts/outfit.tsx
@@ -0,0 +1,6 @@
+import { Outfit } from "next/font/google";
+
+export const outfit = Outfit({
+    weight: ['300', '400'],
+    subsets: ['latin']
+});
--- site/src/app/frequently-asked-questions/page.tsx
+++ site/src/app/frequently-asked-questions/page.tsx
@@ -1,10 +1,23 @@
+import Image from "next/image";
+
 import Navigation from "../components/Navigation";
+
+import { outfit } from "../fonts/outfit";
 
 export default function FAQ () {
     return(
         <div>
             <Navigation/>
-            <p>FAQ</p>
+
+            <div className="relative h-80">
+                <Image className="object-cover w-full" src="/images/FAQ_Banner.png" alt="A photo from our Fall 2023 game showcase" layout="fill"/>
+                
+                <div className="absolute opacity-50 bg-black h-full w-full"></div>
+
+                <h1 className={`absolute left-20 top-32 text-white text-5xl ${outfit.className}`}>Frequently Asked Questions</h1>
+            </div>
+
+            
         </div>
     );
 }
