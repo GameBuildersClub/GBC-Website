d5fc45a9ea8334569e4c94ee19a71c0c056ce2f4
Opuhlos <ericnguyen455@gmail.com>
2025-05-04 18:34:01 -0400

    Implemented responsiveness to faq list

--- site/src/app/data/faq.tsx
+++ site/src/app/data/faq.tsx
@@ -1,4 +1,16 @@
-export const data = [
+export const faq = [
+    {
+        question: "Do I need programming experience to join?",
+        answer: "WIP."
+    },
+    {
+        question: "I want to lead my own game project. Where do I start?",
+        answer: "WIP."
+    },
+    {
+        question: "How big of a commitment are the projects?",
+        answer: "WIP."
+    },
     {
         question: "What game engines are used?",
         answer: "We have used Godot, Unity, and Unreal Engine to create games. However, game engine usage is not limited to these three as it is up to a team leader’s discretion."
--- site/src/app/frequently-asked-questions/page.tsx
+++ site/src/app/frequently-asked-questions/page.tsx
@@ -6,7 +6,7 @@ import Navigation from "../components/Navigation";
 
 import FrequentlyAskedQuestion from "../components/FrequentlyAskedQuestion";
 
-import { data } from "./data";
+import { faq } from "../data/faq";
 
 import { outfit } from "../fonts/outfit";
 import { poppins } from "../fonts/poppins";
@@ -18,6 +18,7 @@ export default function FAQ () {
         <div>
             <Navigation/>
 
+            {/* Banner */}
             <div className="relative h-80">
                 <Image className="object-cover w-full" src="/images/FAQ_Banner.png" alt="A photo from our Fall 2023 game showcase" layout="fill"/>
                 
@@ -27,10 +28,10 @@ export default function FAQ () {
             </div>
 
             {/* Map data of frequently asked questions to the corresponding component */}
-            <div className="flex flex-col mx-72 mt-8">
+            <div className="flex flex-col mt-8 mb-56 mx-5 md:mx-12 xl:mx-96">
                 <p className={`${poppins.className}`}> Below you will find some commonly asked questions about Game Builders Club. If you still have further questions, please reach out to us via Discord or email.</p>
 
-                { data.map((entry, index) => <FrequentlyAskedQuestion question={entry.question} answer={entry.answer} key={index}/> )}
+                { faq.map((entry, index) => <FrequentlyAskedQuestion question={entry.question} answer={entry.answer} key={index}/> )}
             </div>
         </div>
     );
