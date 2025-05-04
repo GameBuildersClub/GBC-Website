9a0d65af41aeebad8a92f0887d5244d4849824cf
Opuhlos <ericnguyen455@gmail.com>
2025-05-04 19:51:27 -0400

    Centered FAQ title and optional rendering between verbose and unverbose

--- site/src/app/frequently-asked-questions/page.tsx
+++ site/src/app/frequently-asked-questions/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 
-import { useState } from "react";
+import { useState, useCallback, useEffect } from "react";
 
 import Navigation from "../components/Navigation";
 
@@ -18,10 +18,12 @@ import { poppins } from "../fonts/poppins";
 export default function FAQ () {
     const [isExpandAllClicked, setIsExpandAllClicked] = useState(false)
 
+    // Handler for expanding all the faq options
     const handleExpandAll = () => {
         setIsExpandAllClicked(!isExpandAllClicked)
-        console.log("click")
     }
+
+    const isBreakPoint = useMediaQuery(500)
 
     return(
         <div>
@@ -34,20 +36,55 @@ export default function FAQ () {
                 {/* Dim Image */}
                 <div className="absolute opacity-50 bg-black h-full w-full"></div>
 
-                <h1 className={`absolute left-20 top-32 text-white text-5xl ${outfit.className}`}>Frequently Asked Questions</h1>
+                {/* "Frequently Asked Questions" or "FAQ" */}
+                {isBreakPoint ? (
+                    <h1 className={`absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl ${outfit.className}`}>FAQ</h1>
+                    ) : (
+                    <h1 className={`absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl ${outfit.className}`}>Frequently Asked Questions</h1>)
+                }
+
             </div>
 
-            {/* Map data of frequently asked questions to the corresponding component */}
+            {/* Body */}
             <div className="flex flex-col mt-8 mb-56 mx-5 md:mx-12 xl:mx-96">
+                {/* Description */}
                 <p className={`${poppins.className}`}> Below you will find some commonly asked questions about Game Builders Club. If you still have further questions, please reach out to us via Discord or email.</p>
 
-                {/* Expand or minimize all faqs */}
+                {/* Expand or minimize all faqs button */}
                 <div className="flex flex-row justify-end my-3">
                     <p className={`cursor-pointer text-primary w-fit hover:underline ${poppins.className}`} onClick={handleExpandAll}>{!isExpandAllClicked ? "EXPAND ALL" : "CLOSE ALL"}</p>
                 </div>
 
+                {/* Map data of frequently asked questions to the corresponding component */}
                 { faq.map((entry, index) => <FrequentlyAskedQuestion expandState={isExpandAllClicked} question={entry.question} answer={entry.answer} key={index}/> )}
             </div>
         </div>
     );
 }
+
+// Logic to render "Frequently Asked Questions" or "FAQ" depending on screen size
+// Reference https://github.com/vercel/next.js/discussions/14810#discussioncomment-8606715
+const useMediaQuery = (width: number) => {
+    const [targetReached, setTargetReached] = useState(false);
+    
+    const updateTarget = useCallback((e: MediaQueryListEvent) => {
+        if (e.matches) {
+        setTargetReached(true);
+        } else {
+        setTargetReached(false);
+        }
+    }, []);
+    
+    useEffect(() => {
+        const media = window.matchMedia(`(max-width: ${width}px)`);
+        media.addEventListener("change", updateTarget);
+    
+        if (media.matches) {
+        setTargetReached(true);
+        }
+    
+        return () => media.removeEventListener("change", updateTarget);
+    }, []);
+    
+    return targetReached;
+};
