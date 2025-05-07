48cb0880554a7de84d029cf9cb599bd94ff246b0
Opuhlos <ericnguyen455@gmail.com>
2025-05-07 12:17:33 -0400

    Image slider/landing carousel functional with transitions. All that is left is responsiveness and eventual image pool randomization

--- site/src/app/components/carousel/LandingCarousel.tsx
+++ site/src/app/components/carousel/LandingCarousel.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 import styles from './styles.module.css';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { outfit } from "@/app/fonts/outfit";
 import { poppins } from "@/app/fonts/poppins";
@@ -13,6 +13,7 @@ import { poppins } from "@/app/fonts/poppins";
 export default function LandingCarousel() {
 
     const [imageIndex, setImageIndex] = useState(0)
+    const [isHover, setIsHover] = useState(false);
    
     const img1 = "/images/Spring_2025_Decked_Out_Group_Photo.png"
     const img2 = "/images/Fall_2023_Showcase 1.png"
@@ -20,17 +21,29 @@ export default function LandingCarousel() {
 
     const  images = [img1, img2, img3]
 
-    const onClickLeft = () => {
-        setImageIndex((imageIndex - 1 + images.length ) % images.length)
-    }
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // Pause the image slider if mouse is hovering over
+            if (isHover) return;
 
-    const onClickRight = () => {
-        setImageIndex((imageIndex + 1) % images.length)
-    }
+            // Increment image index
+            setImageIndex(imageIndex => (imageIndex + 1) % images.length)
+        }, 5000) // Every five seconds
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [isHover])
 
     return(
-        <div className={styles.container}>
-            
+        <div onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} className={styles.container}>
+            {/* Images */}
+            <div className={styles.slider}>
+                {images.map(url => (
+                    <Image key={url} style={{translate: `${-100 * imageIndex}%`}} className={`${styles.image}`} quality={100} loading="eager" src={url} alt="Landing Page Image" width={12000} height={4000}></Image>
+                ))}
+            </div>
+
             <div className={styles.dim}></div>
 
             {/* Text */}
@@ -39,9 +52,20 @@ export default function LandingCarousel() {
                 <h2 className={`${poppins.className} ${styles.h2}`}>Building games together at the University of Georgia since 2021.</h2>
             </div>
 
-            <Image className={styles.image} quality={100} loading="eager" src={images[imageIndex]} alt="Landing Page Image" width={12000} height={4000}></Image>
-            <button className={styles.buttonLeft} onClick={onClickLeft}></button>
-            <button className={styles.buttonRight} onClick={onClickRight}></button>
+            {/* Index Buttons */}
+            <div className={styles.indexContainer}>
+                {images.map((_, index) => (
+                    <button key={index} className={styles.indexButton} onClick={() => setImageIndex(index)} aria-label={`View Image ${index}`}>
+                        {index === imageIndex ? 
+                            <Image className={styles.indexButton} quality={500} loading="eager" src="filled_ellipse.svg" alt="Landing Page Image" width={15} height={15}></Image> 
+                            : 
+                            <Image className={styles.indexButton} quality={500} loading="eager" src="ellipse.svg" alt="Landing Page Image" width={15} height={15}></Image>
+                        }
+                    </button>
+                ))}
+            </div>
+
+            
         </div>
     );
 }
