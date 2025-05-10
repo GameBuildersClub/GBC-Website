f4bf77125a680ffa4459de2b713306723e0d58cd
Opuhlos <ericnguyen455@gmail.com>
2025-05-10 13:52:51 -0400

    Responsive navigation bar and animated hamburger button. No actual functionality for hamburger menu yet

--- site/src/app/components/Navigation.tsx
+++ site/src/app/components/Navigation.tsx
@@ -1,16 +1,18 @@
 'use client'
 
-
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useCallback, useEffect } from "react";
 
-
 import { poppins } from "../fonts/poppins";
+
+import hamburgers from "../../../styles/manifest.module.scss"
 
 // https://github.com/vercel/next.js/discussions/14810#discussioncomment-8606715
 
 export default function Navigation () {
+    const[isHamburgerClicked, setIsHamburgerClicked] = useState(false)
+
     const useMediaQuery = (width: number) => {
         const [targetReached, setTargetReached] = useState(false);
       
@@ -36,32 +38,56 @@ export default function Navigation () {
         return targetReached;
     };
 
+    const handleHamburgerClick = () => {
+      setIsHamburgerClicked(!isHamburgerClicked)
+    }
+
     return(
         <div className="bg-darkGrey flex flex-col w-full">
 
-        <div className="flex flex-row justify-between mx-14">
+        <div className="flex flex-row justify-between mx-5 sm:mx-5 lg:mx-14 md:mx-12">
                 {/* Left Side of Navigation Bar, comprising of logo and links */}
                 <div className="flex flex-row my-3">
                     {/* The svg has some strange artifacts around the edges of the letters which will need to be resolved eventually */}
-                    <Image className="mr-7" priority src="logo.svg" height={60} width={60} alt="GBC Logo"/>
+                    <Link href="/">
+                      <Image className="mr-7" priority src="logo.svg" height={60} width={60} alt="GBC Logo"/>
+                    </Link>
 
                     {/* Navigation Links to different sections of the home page + other pages */}
-                    <div className={`${poppins.className} flex flex-row gap-8 text-white items-center text-lg`}>
-                        <Link href="/"> Games </Link>
-                        <Link href="/"> About Us </Link>
-                        <Link href="/"> How it Works </Link>
-                        <Link href="/frequently-asked-questions"> FAQ </Link>
-                        <Link href="/"> Contact </Link>
-                    </div>
+                    {!useMediaQuery(960) && 
+                      <div className={`${poppins.className} flex flex-row gap-8 text-white items-center text-lg`}>
+                          <Link href="/"> Games </Link>
+                          <Link href="/"> About Us </Link>
+                          <Link href="/"> How it Works </Link>
+                          <Link href="/frequently-asked-questions"> FAQ </Link>
+                          <Link href="/"> Contact </Link>
+                      </div>
+                    }
                 </div>
 
-                {/* Right Side of Navigation Bar, featuring social medias */}
-                {!useMediaQuery(960) && <div className="flex flex-row gap-x-4 items-center">
-                    <Link className="flex-shrink-0" href="https://x.com/GameBuilderClub" target="_blank"> <Image priority src="twitter.svg" height={30} width={30} alt="X (Formerly Twitter) Logo"/> </Link>
-                    <Link className="flex-shrink-0" href="https://www.instagram.com/gamebuildersclub/" target="_blank"> <Image priority src="instagram.svg" height={27} width={27} alt="Instagram Logo"/> </Link>
-                    <Link className="flex-shrink-0" href="https://discord.gg/ZZU5xQbv8K" target="_blank"> <Image priority src="discord.svg" height={35} width={35} alt="Discord Logo"/> </Link>
-                </div>}
+                {/* Right Side of Navigation Bar*/}
+                {!useMediaQuery(960) ? 
+                (
+                  // Social Media Buttons
+                  <div className="flex flex-row gap-x-4 items-center">
+                      <Link className="flex-shrink-0" href="https://x.com/GameBuilderClub" target="_blank"> <Image priority src="twitter.svg" height={30} width={30} alt="X (Formerly Twitter) Logo"/> </Link>
+                      <Link className="flex-shrink-0" href="https://www.instagram.com/gamebuildersclub/" target="_blank"> <Image priority src="instagram.svg" height={27} width={27} alt="Instagram Logo"/> </Link>
+                      <Link className="flex-shrink-0" href="https://discord.gg/ZZU5xQbv8K" target="_blank"> <Image priority src="discord.svg" height={35} width={35} alt="Discord Logo"/> </Link>
+                  </div>
+                ) : (
+                  // Hamburger Menu Button
+                  <button onClick={handleHamburgerClick} className={`${hamburgers["hamburger"]} ${hamburgers["hamburger--spin"]} ${isHamburgerClicked && hamburgers["is-active"]}`} type="button">
+                    <span className={hamburgers["hamburger-box"]}>
+                      <span className={hamburgers["hamburger-inner"]}></span>
+                    </span>
+                </button>  
+                )
+                }
+
+                
             </div>
+
+            
 
             {/* Red Accent */}
             <div className="bg-primary h-1"> </div>
