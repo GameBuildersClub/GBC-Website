205e84b485a7a2fb692650a4285e8afd615cec65
Divesh Gupta <diveshg9@gmail.com>
2026-05-13 04:34:45 -0400

    analytics

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { outfit } from "./fonts/outfit";
 import { poppins } from "./fonts/poppins";
 import { orienta } from "./fonts/orienta";
+import { Analytics } from "@vercel/analytics/next";
 
 const BASE_URL = "https://ugagbc.com";
 const DESC = "The University of Georgia's student-run game development club. We build games, run jams, and teach each other everything from code to art to music.";
@@ -35,7 +36,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${outfit.className} ${poppins.className} ${orienta.variable}`}>{children}</body>
+      <body className={`${outfit.className} ${poppins.className} ${orienta.variable}`}>{children}<Analytics /></body>
     </html>
   );
 }
